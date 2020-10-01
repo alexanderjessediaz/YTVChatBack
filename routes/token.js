88e847fbc469e9fb5c073c5ce86e122ad9f477cf
AJ <alexanderjessediaz@gmail.com>
2020-10-01 16:44:10 -0600

    add routes folder, and token.js file, in token.js add express, router, and TokenService add logic to generate toekn

---
+++ routes/token.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var TokenService = require('../services/tokenService');
+
+// POST /token
+router.post('/', function(req, res) {
+  var identity = req.body.identity;
+
+  var token = TokenService.generate(identity)
+
+  res.json({
+    identity: identity,
+    token: token.toJwt(),
+  });
+});
