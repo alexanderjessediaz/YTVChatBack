72123a4642267ab712af79189ce993b53318bdb2
AJ <alexanderjessediaz@gmail.com>
2020-10-01 16:58:16 -0600

    in token.js add the get token logic

--- routes/token.js
+++ routes/token.js
@@ -13,3 +13,17 @@ router.post('/', function(req, res) {
     token: token.toJwt(),
   });
 });
+// GET /token
+router.get('/', function(req, res) {
+    var identity = req.query.identity;
+  
+    var token = TokenService.generate(identity)
+  
+    res.json({
+      identity: identity,
+      token: token.toJwt(),
+    });
+  });
+  
+  
+  module.exports = router;
